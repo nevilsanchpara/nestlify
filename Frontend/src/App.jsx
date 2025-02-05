56e02e5873a81af13ad31b31f98b7e4c13d898aa
Nevil <nevilsaspara@gmail.com>
2025-02-04 22:04:30 -0500

    Setup the tailwind css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,20 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Nestlify!! </h1>
-      <div className="card">
-        <p>
-          Hii!! Netlify
-        </p>
-      </div>
+      <h1 className="text-3xl font-bold text-blue-600">Hello, Tailwind CSS</h1>
     </>
   )
 }
