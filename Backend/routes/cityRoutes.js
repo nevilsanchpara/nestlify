996b6d62b28f9e482ca1f4bc2f96bf3e34e7284e
poojan0502 <51077428+poojan0502@users.noreply.github.com>
2025-03-06 08:24:30 -0500

    Update cityRoutes.js

--- Backend/routes/cityRoutes.js
+++ Backend/routes/cityRoutes.js
@@ -3,5 +3,7 @@ const { createCity } = require("../controllers/cityController");
 const router = express.Router();
 
 router.post("/", createCity);
+router.get("/", getAllCities);
+router.get("/:id", getCityById);
 
 module.exports = router;
