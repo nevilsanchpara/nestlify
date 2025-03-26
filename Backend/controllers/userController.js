18d9ed5e04bdd139b3375b12b7fe12a1cc4abd10
poojan0502 <poojankheni123@gmail.com>
2025-03-26 02:28:18 -0400

    sdvdsv

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
-
+require("dotenv").config();
 const transporter = nodemailer.createTransport({
   service: "Gmail",
   auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
