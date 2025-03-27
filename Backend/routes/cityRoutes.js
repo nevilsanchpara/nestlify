41746250a9bb129d8d14bcec1337249de6bfeedb
poojan0502 <poojankheni123@gmail.com>
2025-03-27 01:41:54 -0400

    refactor: remove unnecessary whitespace in cityRoutes

--- Backend/routes/cityRoutes.js
+++ Backend/routes/cityRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { createCity, getAllCities,getCityById,deleteCity } = require("../controllers/cityController");
+const { createCity, getAllCities, getCityById, deleteCity } = require("../controllers/cityController");
 const router = express.Router();
 
 router.post("/", createCity);
 router.get("/", getAllCities);
 router.get("/:id", getCityById);
-router.delete("/:id", deleteCity);  
+ 
 
 module.exports = router;
