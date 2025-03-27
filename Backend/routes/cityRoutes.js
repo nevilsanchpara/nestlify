2423dc0a11c3709fce7bd06cd9ddcc82b7b9127d
poojan0502 <poojankheni123@gmail.com>
2025-03-27 01:31:21 -0400

    feat: add getAllCities endpoint and update environment variable for Geoapify API key

--- Backend/routes/cityRoutes.js
+++ Backend/routes/cityRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const { createCity } = require("../controllers/cityController");
+const { createCity, getAllCities } = require("../controllers/cityController");
 const router = express.Router();
 
 router.post("/", createCity);
+router.get("/", getAllCities);
+router.get("/:id", getCityById);
+router.delete("/:id", deleteCity);  
 
 module.exports = router;
